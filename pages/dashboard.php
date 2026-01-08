2091b06f274835522f0b1f15524ae0e249d61849
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 17:41:55 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -634,7 +634,7 @@
                         <span style="color: rgb(74,86,101);">Due: Feb 15, 2025</span><br>
                         <span style="font-size: 14px;"><i class="bi bi-people"></i>&nbsp;Jane Brown</span><br>
                           <span style="font-size: 14px; color: rgb(243,36,57);"><i class="bi bi-calendar2"></i>&nbsp;Apr 30, 2025</span><br>
-                          <span class="badge text-bg-secondary" style="background-color: rgba(235, 236, 237, 1) !important; color: rgb(57,69,85) !important; font-weight: 500 !important;">SOC 2 Type 2</span><br>
+                          <span class="badge text-bg-secondary" style="background-color: rgba(235, 236, 237, 1) !important; color: rgb(57,69,85) !important; font-weight: 500 !important;">SOC 2 Type 2</span>
                           <span class="badge text-bg-danger" style="background-color: rgb(255,226,226) !important; color: rgb(201,0,18) !important;font-weight: 500 !important;">Overdue</span>
                       </p>
 
