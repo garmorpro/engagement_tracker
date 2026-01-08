6cdb633714afce1589d306f843ecf16be72d7258
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 13:00:40 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -213,7 +213,7 @@
       <span class="input-group-text bg-white border-end-0">
         <i class="bi bi-search"></i>
       </span>
-      <input type="text" class="form-control border-start-0" placeholder="Search...">
+      <input type="text" class="form-control border-start-0" style="background-color: pink;" placeholder="Search...">
     </div>
 
     <!-- Filter button -->
