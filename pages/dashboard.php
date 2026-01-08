09f897ed591b9d3e65f24280bc35e21a0091dbc7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 13:50:29 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -239,8 +239,8 @@
 
         <!-- Header -->
         <div class="d-flex align-items-center mb-3">
-          <div class="icon-square bg-danger me-2">
-            <i class="bi bi-exclamation-circle" style="color: rgb(241,0,24); background-color: rgb(255,241,242);"></i>
+          <div class="icon-square bg-danger me-2" style="background-color: rgb(255,241,242);">
+            <i class="bi bi-exclamation-circle" style="color: rgb(241,0,24);"></i>
           </div>
           <h6 class="fw-semibold mb-0">Overdue</h6>
         </div>
