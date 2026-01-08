c4677fec8cb917c59f0a88a3d28da18ce3c7de9a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 14:32:42 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -562,9 +562,7 @@
                 <div class="card-body">
                     <!-- <h5 class="card-title">Card title</h5> -->
                      <span class="badge rounded-pill text-bg-primary">
-                        <h6>
-                            On Hold
-                        </h6>
+                        On Hold
                     </span>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
