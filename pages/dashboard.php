9304dbbbf168363ee83e3632a1d98cd20345bace
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 17:43:00 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -629,13 +629,13 @@
                       </div>
 
                       <!-- Subtext -->
-                      <p class="text-secondary" style="font-size: 14px; margin-bottom: -5px !important;">
-                        <span style="color: rgb(106,115,130);">ENG-2024-001</span><br>
+                      <p class="text-secondary" style="font-size: 16px; margin-bottom: -5px !important;">
+                        <span style="color: rgb(106,115,130); font-size: 14px !important;">ENG-2024-001</span><br>
                         <span style="color: rgb(74,86,101);">Due: Feb 15, 2025</span><br>
                         <span style="font-size: 14px;"><i class="bi bi-people"></i>&nbsp;Jane Brown</span><br>
-                          <span style="font-size: 14px; color: rgb(243,36,57);"><i class="bi bi-calendar2"></i>&nbsp;Apr 30, 2025</span><br>
-                          <span class="badge text-bg-secondary" style="background-color: rgba(235, 236, 237, 1) !important; color: rgb(57,69,85) !important; font-weight: 500 !important;">SOC 2 Type 2</span>
-                          <span class="badge text-bg-danger" style="background-color: rgb(255,226,226) !important; color: rgb(201,0,18) !important;font-weight: 500 !important;">Overdue</span>
+                        <span style="font-size: 14px; color: rgb(243,36,57);"><i class="bi bi-calendar2"></i>&nbsp;Apr 30, 2025</span><br>
+                        <span class="badge text-bg-secondary" style="background-color: rgba(235, 236, 237, 1) !important; color: rgb(57,69,85) !important; font-weight: 500 !important;">SOC 2 Type 2</span>
+                        <span class="badge text-bg-danger" style="background-color: rgb(255,226,226) !important; color: rgb(201,0,18) !important;font-weight: 500 !important;">Overdue</span>
                       </p>
 
                     </div>
