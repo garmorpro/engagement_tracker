85900121afc7eeda45b454023ed4ba1ba26f6726
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 08:50:09 -0600

    update

--- includes/auth.php
+++ includes/auth.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Log successful login
             logActivity($conn, "successful_login", $user_id, $account_name, "Service Account Login", "Successful login");
             $error = "Success!";
-            // header("Location: ../pages/service-settings.php");
+            header("Location: success.php");
             exit;
         } else {
             logActivity($conn, "failed_login", $user_id, $account_name, "Failed Login", "Incorrect password");
