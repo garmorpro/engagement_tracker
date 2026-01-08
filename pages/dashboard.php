532c7eb267d276bdd304e1c1d4b4bc6c59161ce6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 13:03:55 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -210,10 +210,10 @@
   <div class="p-3 border rounded d-flex align-items-center" style="box-shadow: 1px 1px 4px rgba(0,0,0,0.15);">
     
     <div class="input-group flex-grow-1 me-3">
-      <span class="input-group-text border-end-0" style="background-color: pink;">
+      <span class="input-group-text border-end-0" style="background-color: rgb(248,249,251);">
         <i class="bi bi-search"></i>
       </span>
-      <input type="text" class="form-control border-start-0" style="background-color: pink;" placeholder="Search...">
+      <input type="text" class="form-control border-start-0" style="background-color: rgb(248,249,251);;" placeholder="Search...">
     </div>
 
     <!-- Filter button -->
