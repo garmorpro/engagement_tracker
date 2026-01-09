f68bf818ca847b522cf6293b3e6bfc324e3ab394
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 23:30:37 -0600

    update

--- pages/engagement-timeline.php
+++ pages/engagement-timeline.php
@@ -234,6 +234,13 @@ require_once '../includes/functions.php';
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
+  <?php
+echo '<pre>';
+print_r($engagements);
+echo '</pre>';
+exit;
+?>
+
   <script>
 const engagements = <?php
 if (!empty($engagements)) {
