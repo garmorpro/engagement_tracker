4cc0373d8957ddb9324e7d63a1c55ab0fdfa9190
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-09 12:30:45 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -666,6 +666,7 @@ require_once '../includes/functions.php';
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js"></script>
+  <script src="../assets/js/sortable.js"></script>
 
 
 </body>
