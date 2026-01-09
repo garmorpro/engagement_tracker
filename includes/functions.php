545d5a7dd4d7bb94da2de7986c642a747e3b86ac
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-09 17:38:14 -0600

    update

--- includes/functions.php
+++ includes/functions.php
@@ -10,7 +10,7 @@ session_start();
 function getAllEngagements($conn) {
     $sql = "SELECT `eng_id`, `eng_idno`, `eng_name`, `eng_manager`, `eng_senior`, `eng_staff`, 
                    `eng_senior_dol`, `eng_staff_dol`, `eng_poc`, `eng_location`, `eng_repeat`, 
-                   `eng_audit_type`, `eng_scope`, `eng_tsc`, `eng_period`, `eng_as_of_date`, 
+                   `eng_audit_type`, `eng_scope`, `eng_tsc`, `eng_start_period`, `eng_end_period`, `eng_as_of_date`, 
                    `eng_irl_due`, `eng_client_planning_call`, `eng_scheduled_client_planning`, 
                    `eng_fieldwork`, `eng_leadsheet_start`, `eng_leadsheet_due`, `eng_draft_due`, 
                    `eng_final_due`, `eng_archive`, `eng_section_3_requested`, `eng_last_communication`, 
