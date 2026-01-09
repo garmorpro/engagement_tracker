5fb1c3cb8f5fb32bf91416c8e9ca775677d0d1c0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-08 19:16:29 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -159,7 +159,7 @@
 
 .table-wrapper table.table-hover tbody tr:hover > td,
 .table-wrapper table.table-hover tbody tr:hover > th {
-  background-color: rgba(26, 26, 164, 1) !important;
+  background-color: rgba(246, 246, 247, 0.6) !important;
 }
 
   </style>
