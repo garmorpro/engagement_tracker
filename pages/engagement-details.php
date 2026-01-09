5d2dd1f1fab40ffa3f9ddd78cc2766fa7814b9a4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-09 11:34:12 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -56,12 +56,14 @@ require_once '../includes/functions.php';
   <div style="margin-left: 200px; margin-right: 200px;">
     <div class="d-flex justify-content-between">
       <div class="left">
-        <div class="d-flex align-items-center mb-3">
-          <div class="icon-square me-2" style="height: 30px; width: 30px; background-color: rgb(255,255,255); border: 1px solid rgb(229,231,235);">
-            <i class="bi bi-arrow-left" style="color: rgb(84,96,110);"></i>
-          </div>
-          <p class="mb-0" style="font-size: 14px;">Back to Dashboard</p>
-        </div>
+        <a href="dashboard.php" class="back-link text-decoration-none">
+  <div class="d-flex align-items-center mb-3 back-link-inner">
+    <div class="icon-square me-2 d-flex align-items-center justify-content-center">
+      <i class="bi bi-arrow-left"></i>
+    </div>
+    <p class="mb-0">Back to Dashboard</p>
+  </div>
+</a>
       </div>
       <div class="right">
         endswitch
