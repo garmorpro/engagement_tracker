c4333ee935ca3784ab1ac54a11c854da5f2c0d06
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-09 00:00:12 -0600

    update

--- pages/engagement-timeline.php
+++ pages/engagement-timeline.php
@@ -45,8 +45,8 @@ $engagements = getAllEngagements($conn);
             </a>
           </div>
 
-          <button class="btn menu-btn btn-sm ms-3"><i class="bi bi-archive"></i>&nbsp;&nbsp;Archive</button>
-          <button class="btn menu-btn btn-sm ms-3"><i class="bi bi-tools"></i>&nbsp;&nbsp;Tools</button>
+          <a class="btn menu-btn btn-sm ms-3" href="archive.php"><i class="bi bi-archive"></i>&nbsp;&nbsp;Archive</a>
+          <a class="btn menu-btn btn-sm ms-3" href="tools.php"><i class="bi bi-tools"></i>&nbsp;&nbsp;Tools</a>
           <button class="btn new-btn btn-sm ms-3"><i class="bi bi-plus"></i>&nbsp;&nbsp;New Engagement</button>
         </div>
       </div>
