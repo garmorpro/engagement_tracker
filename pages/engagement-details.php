91a983a1808541c278ea08b376ebdce953dd1626
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-10 02:08:22 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -347,7 +347,7 @@ $pillColor = $statusColors[$status]['pill'] ?? '#000';
           <div class="icon-square me-2" style="background-color: rgb(222,234,252); height: 40px; width: 40px;">
             <i class="bi bi-people" style="color: rgb(0,42,241);"></i>
           </div>
-          <h6 class="fw-semibold mb-0" style="color: rgb(0,0,123);">Team</h6>
+          <h6 class="fw-semibold mb-0" style="color: rgb(0,0,123); font-size: 16px;">Team</h6>
         </div>
 
         <div class="card mb-4" style="border-color: rgb(214,232,254); border-radius: 15px; background-color: rgb(226,238,253);">
