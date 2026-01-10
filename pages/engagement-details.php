3339cf9915cbd92fa32ef6f20048ffea3b11a760
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-10 01:59:29 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -362,7 +362,7 @@ $pillColor = $statusColors[$status]['pill'] ?? '#000';
       <div class="card mb-3" style="border-color: rgb(229,231,235); border-radius: 15px; background-color: rgb(255,255,255);">
         <div class="card-body p-4">
           <div class="d-flex align-items-center mb-3">
-            <div class="icon-square me-2" style="background-color: rgb(33,128,255);">
+            <div class="icon-square me-2" style="background-color: rgb(33,128,255); height: 25px; width: 25px;">
               <i class="bi bi-exclamation-circle" style="color: rgb(255,255,255);"></i>
             </div>
             <h6 class="fw-semibold mb-0" style="color: rgb(0,0,123);">Trust Services Criteria</h6>
