773f7407d9c6141c0c8b9ac23bc3758590fd2771
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-10 02:45:37 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -357,7 +357,7 @@ $pillColor = $statusColors[$status]['pill'] ?? '#000';
               <h6 class="mb-0" style="color: rgb(21,87,242); font-weight: 500 !important; font-size: 14px !important;">Manager</h6>
             </div>
             <h6 class="fw-semibold" style="color: rgb(0,37,132); font-size: 20px;">
-              <?php echo htmlspecialchars($eng['eng_manager'] ?? 'N/A'); ?>
+              <?php echo htmlspecialchars($eng['eng_manager'] ?? 'Manager not assigned'); ?>
             </h6>
           </div>
         </div>
@@ -372,7 +372,7 @@ $pillColor = $statusColors[$status]['pill'] ?? '#000';
               <?php echo htmlspecialchars($eng['eng_senior'] ?? 'Senior not assigned'); ?>
             </h6>
             <p class="pt-2" style="color: rgb(97,0,206); font-size: 12px;">
-              <strong>DOL:</strong> <?php echo htmlspecialchars($eng['eng_senior_dol'] ?? 'N/A'); ?>
+              <strong>DOL:</strong> <?php echo htmlspecialchars($eng['eng_senior_dol'] ?? 'DOL not assigned'); ?>
             </p>
           </div>
         </div>
@@ -387,7 +387,7 @@ $pillColor = $statusColors[$status]['pill'] ?? '#000';
               <?php echo htmlspecialchars($eng['eng_staff'] ?? 'Staff not assigned'); ?>
             </h6>
             <p class="pt-2" style="color: rgb(0,142,0); font-size: 12px;">
-              <strong>DOL:</strong> <?php echo htmlspecialchars($eng['eng_staff_dol'] ?? 'N/A'); ?>
+              <strong>DOL:</strong> <?php echo htmlspecialchars($eng['eng_staff_dol'] ?? 'DOL not assigned'); ?>
             </p>
           </div>
         </div>
