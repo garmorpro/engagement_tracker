2883b1856c2b078bc12c593c8a97792dee133f09
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-10 20:26:51 -0600

    update

--- pages/engagement-list.php
+++ pages/engagement-list.php
@@ -341,8 +341,12 @@ $totalEngagements = count($engagements);
      TEXT FIELDS
 ===================== -->
 
+<h6 style="fw-semibold mt-4">
+  Basic Information
+</h6>
+
 <div class="col-md-6">
-  <label class="form-label">Engagement Name</label>
+  <label class="form-label">Engagement Name <sub>*</sub></label>
   <input type="text" class="form-control" name="eng_name"
          value="<?php echo htmlspecialchars($eng['eng_name'] ?? '', ENT_QUOTES); ?>">
 </div>
