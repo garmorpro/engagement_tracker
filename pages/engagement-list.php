9060dad368695d1a9d14004ef06dc86572139b84
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-10 22:29:55 -0600

    update

--- pages/engagement-list.php
+++ pages/engagement-list.php
@@ -361,8 +361,8 @@ $checked = (($eng['eng_repeat'] ?? 'N') === 'Y');
 
     <div class="yn-toggle <?php echo $checked ? 'active' : ''; ?>"
          onclick="toggleYN(this)">
-      <?php echo $checked ? '✓ Y' : 'N'; ?>
-      <br><br> <span style="font-size: 8px;">Repeat Client</span>
+      <?php echo $checked ? '✓ Y' : '<span style="font-size: 8px;">Repeat Client</span>'; ?>
+      <!-- <br><br>  -->
     </div>
 
     <!-- ALWAYS POST -->
