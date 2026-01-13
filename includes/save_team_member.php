d99cecdcef1021012a978272b12b035d85694ae6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-12 21:05:23 -0600

    update

--- includes/save_team_member.php
+++ includes/save_team_member.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-include '../includes/db.php'; // your DB connection
+include 'db.php'; // your DB connection
 
 $eng_id = (int)($_POST['eng_id'] ?? 0);
 $type = $_POST['type'] ?? '';
@@ -14,7 +14,6 @@ if (!in_array($type, ['senior', 'staff']) || $index < 1 || $index > 2 || $eng_id
 }
 
 $column = 'eng_' . $type . $index; // e.g., eng_senior1
-echo $column;
 $stmt = $conn->prepare("UPDATE engagements SET `$column` = ? WHERE eng_idno = ?");
 $stmt->bind_param("si", $name, $eng_id);
 $success = $stmt->execute();
