c51ce44a99bec7694b63a6985aee0ad8c6c819f3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-14 14:04:35 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -786,7 +786,7 @@ $totalEngagements = count($engagements);
 <?php include_once '../includes/modals/adding_engagement_modal.php'; ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <!-- <script src="../assets/js/engagement_status.js"></script>  -->
+  <script src="../assets/js/engagement_status.js"></script> 
 
 <script>
     document.addEventListener('DOMContentLoaded', () => {
