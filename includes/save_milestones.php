849bb39e0c5f9bf7d3875fc18e5c34a4f829a915
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-14 14:48:32 -0600

    update

--- includes/save_milestones.php
+++ includes/save_milestones.php
@@ -1,21 +1,30 @@
 <?php
-include 'db.php';
-header('Content-Type: application/json'); // important
+include '../db.php';
+header('Content-Type: application/json');
 
-$response = ['success' => false];
+// Turn off error output to browser
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
 
-$eng_id = $_POST['eng_id'] ?? 0;
+$response = ['success' => false, 'error' => 'Unknown error'];
+
+$eng_id = isset($_POST['eng_id']) ? (int)$_POST['eng_id'] : 0;
 $due_dates = $_POST['due_date'] ?? [];
 
 if ($eng_id && !empty($due_dates)) {
     $stmt = $conn->prepare("UPDATE engagement_milestones SET due_date = ? WHERE ms_id = ? AND eng_id = ?");
-    foreach ($due_dates as $ms_id => $date) {
-        $stmt->bind_param("sii", $date, $ms_id, $eng_id);
-        $stmt->execute();
+    if ($stmt) {
+        foreach ($due_dates as $ms_id => $date) {
+            $stmt->bind_param("sii", $date, $ms_id, $eng_id);
+            $stmt->execute();
+        }
+        $stmt->close();
+        $response['success'] = true;
+        unset($response['error']);
+    } else {
+        $response['error'] = $conn->error;
     }
-    $response['success'] = true;
 }
 
-// Make sure NOTHING else is printed!
 echo json_encode($response);
 exit;
