f2b2b0eecfe9f41f4f9b14633cde2a24299f608b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-16 22:39:12 -0600

    update

--- mobile/index.php
+++ mobile/index.php
@@ -7,6 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
+
+<!-- Bootstrap JS bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
     <link rel="stylesheet" href="assets/css/mobile.css?v=<?php echo time(); ?>">
 
     <title>Document</title>
