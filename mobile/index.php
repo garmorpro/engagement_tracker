c0b46bec0450b432707ab48a116a0589387c7626
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-16 21:55:16 -0600

    update

--- mobile/index.php
+++ mobile/index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
     <link rel="stylesheet" href="../assets/styles/main.css?v=<?php echo time(); ?>">
-    
+
     <title>Document</title>
 </head>
 <body>
@@ -24,7 +24,7 @@
     </div>
 
     <!-- Right -->
-    <div class="header-stat">
+    <div class="header-stat text-end">
       <div class="header-stat-number">12</div>
       <div class="header-stat-text">Active</div>
     </div>
