b6037f03457197dbaed815dbfefbf217d6242552
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-16 21:55:01 -0600

    update

--- mobile/index.php
+++ mobile/index.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tools - Engagement Tracker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../assets/styles/main.css?v=<?php echo time(); ?>">
+    
     <title>Document</title>
 </head>
 <body>
