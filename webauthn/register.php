71f55fff50063be70a65793737c65256f48da2fd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-23 07:43:24 -0600

    update

--- webauthn/register.php
+++ webauthn/register.php
@@ -7,7 +7,8 @@ error_reporting(E_ALL);
 
 require_once '../includes/functions.php';
 // require_once __DIR__ '/../includes/db.php';
-require __DIR__ . '/../../../../vendor/autoload.php';
+require '/var/www/engagement_tracker/vendor/autoload.php';
+
 
 
 
