ca797c7c792596ad4488aba1db850a20501d98bf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-23 07:14:26 -0600

    update

--- assets/webauthn/register.php
+++ assets/webauthn/register.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/includes/init.php';
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Webauthn\PublicKeyCredentialCreationOptions;
 use Webauthn\PublicKeyCredentialRpEntity;
