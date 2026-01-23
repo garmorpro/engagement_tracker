1ca759b31ae8e91ccc06ff997999f27dd4b17126
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-01-23 14:09:05 -0600

    update

--- auth/authentik_redirect.php
+++ auth/authentik_redirect.php
@@ -4,7 +4,7 @@ session_start();
 
 // Replace these with your Authentik settings
 $client_id = 'dekMyHfssWUpwBzKa42Nbfxw2OfJl8TTe78JWK7A';
-$redirect_uri = 'https://et.morganserver.com/pages/dashboard';
+$redirect_uri = 'https://et.morganserver.com/pages/dashboard.php';
 $auth_url = 'http://10.10.254.198:9000/application/o/authorize/';
 
 $state = bin2hex(random_bytes(16)); // Prevent CSRF attacks
