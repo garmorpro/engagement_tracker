2eb1b920f09a53cb8924a0767a2c6e5a13b8a3eb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-03-02 21:05:22 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -93,8 +93,7 @@ if (!$engagement) {
 
         .page-header.scrolled {
             box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-            left: 50%;
-    right: 50%;
+          
     margin-left: -50vw;
     margin-right: -50vw;
         }
