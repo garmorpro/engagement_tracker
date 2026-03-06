e3d3a699da5d42acb73c6316fd7640290ce35243
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-03-05 22:05:54 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -38,6 +38,7 @@ function getTimelineByEngagement($conn, $engagementId) {
 $timeline = getTimelineByEngagement($conn, $engagementId);
 
 echo $timeline['engagement_idno'];
+echo $engagementId;
 
 
 if (!$engagement) {
