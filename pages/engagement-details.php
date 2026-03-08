8291f41d355081e7e1f03253fc09eb85450b222d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-03-07 20:49:15 -0600

    update

--- pages/engagement-details.php
+++ pages/engagement-details.php
@@ -2949,7 +2949,7 @@ document.getElementById('manageTeamIconBtn').addEventListener('click', function(
         html: teamHTML,
         showConfirmButton: false,
         cancelButtonText: 'Close',
-        width: '1000px',
+        width: '1200px',
         heightAuto: false,
         customClass: {
             popup: 'milestone-modal-popup'
