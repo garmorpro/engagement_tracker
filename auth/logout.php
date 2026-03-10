00d4dd574a4c0354108755735e34ac6a667a6a5c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-03-10 10:10:46 -0700

    update

--- auth/logout.php
+++ auth/logout.php
@@ -1,7 +1,10 @@
 <?php
-// auth/logout.php
-session_start();
-require_once '../includes/db.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once __DIR__ . '/../includes/db.php';
+require_once __DIR__ . '/../path.php'; // Make sure this file defines BASE_URL
 
 if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
