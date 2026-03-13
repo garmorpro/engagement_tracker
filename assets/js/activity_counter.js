64c3f050046da0f3fbecd4aa74b959b2bb341ede
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-03-13 08:41:44 -0500

    update

--- assets/js/activity_counter.js
+++ assets/js/activity_counter.js
@@ -1,22 +1,22 @@
-(function() {
-    const INACTIVITY_LIMIT = 15 * 60 * 1000; // 15 minutes
-    let inactivityTimer;
+// (function() {
+//     const INACTIVITY_LIMIT = 15 * 60 * 1000; // 15 minutes
+//     let inactivityTimer;
 
-    function resetTimer() {
-        clearTimeout(inactivityTimer);
-        inactivityTimer = setTimeout(logoutUser, INACTIVITY_LIMIT);
-    }
+//     function resetTimer() {
+//         clearTimeout(inactivityTimer);
+//         inactivityTimer = setTimeout(logoutUser, INACTIVITY_LIMIT);
+//     }
 
-    function logoutUser() {
-        fetch(BASE_URL + '/auth/logout.php')
-            .finally(() => {
-                window.location.href = BASE_URL + '/?timeout=1';
-            });
-    }
+//     function logoutUser() {
+//         fetch(BASE_URL + '/auth/logout.php')
+//             .finally(() => {
+//                 window.location.href = BASE_URL + '/?timeout=1';
+//             });
+//     }
 
-    ['mousemove', 'keydown', 'scroll', 'click'].forEach(evt => {
-        document.addEventListener(evt, resetTimer);
-    });
+//     ['mousemove', 'keydown', 'scroll', 'click'].forEach(evt => {
+//         document.addEventListener(evt, resetTimer);
+//     });
 
-    resetTimer();
-})();
+//     resetTimer();
+// })();
